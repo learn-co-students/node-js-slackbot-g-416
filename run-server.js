99d84deee5f48c60cd91316bd1c9f3b9ef0978fd
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-10-07 20:57:21 -0400

    Done.

---
+++ run-server.js
@@ -0,0 +1,5 @@
+const server = require('./slack_bot');
+
+server.listen(3000, () => {
+  console.log(`app listening on port 3000!`);
+});
--- slack_bot.js
+++ slack_bot.js
@@ -3,13 +3,68 @@
 const rp = require('request-promise');
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('isomorphic-fetch');
 const app = express();
 
-const TOKEN = '[Your Slash Command Token Here]';
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Just an example request to get you started..
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const TOKEN = '8cVJhp6TA0fU1gEsFawEussX';
+
+const validRequest = (token) => {
+  return TOKEN == token;
+};
+
+const prepareResponse = (info, paramToGet) => {
+  let response = { response_type: "ephemeral", mrkdwn: true };
+  const newLine = '\n';
+  response.text = '*Github User: @' + info.login + ' (' + info.name + ')*:' + newLine;
+  if (!paramToGet) {
+    response.text += '> Company: ' + info.company + newLine;
+    response.text += '> Location: ' + info.location + newLine;
+    response.text += '> Hireable: ' + info.hireable + newLine;
+    response.text += '> Github Profile: ' + info.html_url + newLine;
+  }
+  else {
+    response.text += '> ' + paramToGet.charAt(0).toUpperCase() + paramToGet.slice(1) + ': ';
+    response.text += info[paramToGet];
+  }
+  return response;
+};
+
+app.get('/', (req,res) => {
+  res.send('ok');
+});
+
+app.post('/', (req, res) => {
+  if (!validRequest(req.body.token)) {
+    res.status(400).send();
+    return;
+  }
+  if (!req.body.text) {
+    res.status(400).send({
+      response_type: 'ephemeral',
+      text: "Please specify a user to find."
+    });
+    return;
+  }
+  const cmd = req.body.text.split(' '),
+        user = cmd[0],
+        paramToGet = cmd[1];
+  fetch(`https://api.github.com/users/${username}`).then((resp) => {
+    const result = JSON.parse(resp);
+    res.send(prepareResponse(result, paramToGet));
+  }).catch((err) => {
+    let errMsg = { response_type: "ephemeral" };
+    if('statusCode' in err && err.statusCode == 404) {
+      errMsg.text = "Sorry. Unable to find that user.";
+      res.status(err.statusCode).send(errMsg);
+    }
+    else {
+      const status = err.statusCode ? err.statusCode : 500;
+      errMsg.text = "Oops! Something went wrong. Please try again.";
+      res.status(status).send(errMsg);
+    }
+  });
 });
 
 // This code "exports" a function 'listen` that can be used to start
